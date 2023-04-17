02f81078f9097b3732c66edcd19ce1146a4069b5
ise-208 <seed.0ri00@gmail.com>
2023-04-17 22:11:37 +0900

    loggerの追加

--- src/main/java/com/ise/springjpasample/UserController.java
+++ src/main/java/com/ise/springjpasample/UserController.java
@@ -2,10 +2,13 @@ package com.ise.springjpasample;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
 public class UserController {
     UserRepository userRepository;
+    Logger log = LoggerFactory.getLogger(UserController.class);
 
     public UserController(UserRepository userRepository) {
         this.userRepository = userRepository;
@@ -13,6 +16,7 @@ public class UserController {
 
     @GetMapping("/hello")
     public void Hello() {
+        log.info("helloの実施");
         System.out.println(userRepository.findAll());
     }
 }
